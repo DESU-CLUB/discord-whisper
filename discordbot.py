e587013a45f74c48a9733c6c535fb31be59d5389
DESU CLUB <warrenlowkh@gmail.com>
2023-07-08 11:51:44 +0800

    Added base pipeline for summarization
    Right now the summary is done by a FakeLLM (meaning it is still unable to summarize)
    
    TODO:
    Implement actual pipeline with GPT API
    Test whether the text splitter can chunk >context length inputs

--- discordbot.py
+++ discordbot.py
@@ -5,19 +5,48 @@ import os
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from whisper_jax import FlaxWhisperPipline
-import io
-import numpy as np
-import wave
+import tempfile
 import jax.numpy as jnp
 import time
+from langchain import OpenAI, PromptTemplate, LLMChain
+from langchain.llms.fake import FakeListLLM
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.mapreduce import MapReduceChain
+from langchain.chains.summarize import load_summarize_chain
+import tiktoken
+from langchain.docstore.document import Document
 intents = discord.Intents.all()
 intents.members = True
 
+#llm = OpenAI(temperature=0)
+llm = FakeListLLM(responses = ['Able to run', "it ha been a log time,"])
 model =FlaxWhisperPipline("openai/whisper-large-v2", dtype=jnp.float16)
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 executor = ThreadPoolExecutor(max_workers=5)
+content_string = '''\
+You are SummarizeGPT, a LLM that summarizes long lengths of discord dialogues into their main ideas.
+Summarize the following dialogue: 
+{text}
 
+TLDR:'''
+content_template = PromptTemplate(input_variables=['text'], template=content_string)
+
+
+#Tiktoken function to count no of tokens
+def count_tokens(text):
+    tokenizer = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+template_len = count_tokens(content_string)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=8000 - template_len,
+    chunk_overlap=100,
+    length_function=count_tokens,
+    separators=["\n\n","\n",'']
+)
+
+                           
 
 class MyHelp(commands.HelpCommand):
     async def send_bot_help(self, mapping):
@@ -31,7 +60,7 @@ class MyHelp(commands.HelpCommand):
         channel = self.get_destination()
         await channel.send(embed=embed)
 
-        
+
     async def send_command_help(self, command):
         embed = discord.Embed(title=self.get_command_signature(command), color=discord.Color.random())
         if command.help:
@@ -48,34 +77,23 @@ bot.help_command = MyHelp()
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-
-
-
 async def start_recording(ctx,sink,my_callback,*args):
-    ctx.voice_client.start_recording(sink,my_callback,ctx,args)
+    ctx.voice_client.start_recording(sink,my_callback,ctx,*args)
     return
 
-def transcribe(audio):
+def transcriber(audio):
     start = time.time()
     text = model(audio)
-    print('FInish transcription', time.time()-start)
+    print('Finish transcription', time.time()-start)
     os.unlink(audio)
     return text['text']
-
-
-import io
-import numpy as np
-import wave
-
-import io
-import os
-import tempfile
-
-
 
 async def my_callback(sink, *args):
         # Save the recorded audio to a file
         ctx,summarize = args
+        print(summarize)
+        if summarize == 'True':
+            summarize = True
         loop = asyncio.get_event_loop()
         start = time.time()
         progress = await ctx.send('Reading bytes...')
@@ -84,9 +102,14 @@ async def my_callback(sink, *args):
         print('Reading bytes',time.time()-start)
         await progress.edit('Transcribing...')
         start = time.time()
-        text = await loop.run_in_executor(executor,transcribe,data)
+        text = await loop.run_in_executor(executor,transcriber,data)
         print('Transcribed', time.time()-start)
         #If more than 4000 words, split into chunks
+        if summarize == True:
+            await progress.edit('Summarizing...')
+            start = time.time()
+            text = await summarize_text(text)
+            print('Summarized', time.time()-start)
         if len(text) > 4000:
             for x in range(0, len(text), 4000):
                 if x == 0:
@@ -121,11 +144,25 @@ def read_file(sink):
         print(f'Error in read_file: {e}')
         return None
 
+##Count tokens, then summarize
+async def summarize_text(text):
+    texts = text_splitter.split_text(text)
+    docs = [Document(page_content= text) for text in texts]
+    print(docs, docs[0])
+    chain = load_summarize_chain(llm, chain_type='stuff', prompt=content_template)
+    results = await chain.arun(docs)
+    print('Summarization ran')
+    print(results)
+    return ''.join(results) if len(docs) == 1 else '\n\n'.join(results)
+
+async def async_generate(chain,doc):
+    print(doc.page_content)
+    resp = await chain.arun(doc)
+    return resp
 
 
-
-@bot.command(help = "Record audio in a voice channel")
-async def record(ctx):
+@bot.command(help = "Record audio in a voice channel\nSummarize flag: Default=> False,\n True=> Summarize the text")
+async def transcribe(ctx,summarize = False):
         #delete file        
     if ctx.author.voice is None:
         await ctx.send("You are not in a voice channel!")
@@ -145,14 +182,14 @@ async def record(ctx):
     elif ctx.voice_client and ctx.voice_client.is_connected():
         print('Working!')
         sink = WaveSink()
-        await start_recording(ctx,sink,my_callback)
+        await start_recording(ctx,sink,my_callback,summarize)
         return
     else:
         await ctx.send('Unknown Error Found!')
         return
 
 @bot.command(help = "Stop recording audio in a voice channel, and transcribes it")
-async def stop_record(ctx):
+async def stop(ctx):
     if ctx.voice_client is None:
         await ctx.send("I am not in a voice channel!")
         return
@@ -177,3 +214,6 @@ if __name__ == "__main__":
     with open('mybot.txt','r') as f:
         key = f.read().strip()
         bot.run(key)
+
+
+    
